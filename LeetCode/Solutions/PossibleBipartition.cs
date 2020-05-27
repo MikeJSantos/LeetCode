ff04f48b75e3da48b9ccc42fecc33b7d96fe757d
Michael Santos <michael.joseph.santos@gmail.com>
2020-05-27 15:20:32 -0400

    PossibleBipartition accepted

--- LeetCode/Solutions/PossibleBipartition.cs
+++ LeetCode/Solutions/PossibleBipartition.cs
@@ -7,52 +7,65 @@ namespace LeetCode
 {
     public partial class Solution
     {
+        // TODO: Optimize? Beats 30.95% of submissions, 28ms slower than median
+        // (https://leetcode.com/submissions/detail/345516225/)
         public bool PossibleBipartition(int N, int[][] dislikes)
         {
             if (N <= 1 || dislikes == null || dislikes.Length == 0)
                 return true;
 
-            // Build graph dictionary. Key: person1, Value: people person1 is connected to
+            // Build adjacency dictionary. Key: personA, Value: people personA is connected to
             var adjacencyDictionary = new Dictionary<int, List<int>>();
 
             for (var i = 0; i < dislikes.Length; i++)
             {
-                var person1 = dislikes[i][0];
-                var person2 = dislikes[i][1];
+                var personA = dislikes[i][0];
+                var personB = dislikes[i][1];
 
-                if (!adjacencyDictionary.ContainsKey(person1))
-                    adjacencyDictionary[person1] = new List<int>();
-                adjacencyDictionary[person1].Add(person2);
+                if (!adjacencyDictionary.ContainsKey(personA))
+                    adjacencyDictionary[personA] = new List<int>();
+                adjacencyDictionary[personA].Add(personB);
 
-                if (!adjacencyDictionary.ContainsKey(person2))
-                    adjacencyDictionary[person2] = new List<int>();
-                adjacencyDictionary[person2].Add(person1);
+                if (!adjacencyDictionary.ContainsKey(personB))
+                    adjacencyDictionary[personB] = new List<int>();
+                adjacencyDictionary[personB].Add(personA);
             }
 
             // Zero index ignored to keep consistency with N range: {1 ... N}
             var wasVisited = new bool[N+1];
-            // null: no group, false: group A, true: group B
+            // null: no group, false: 1st group, true: 2nd group
             var inGroup  = new bool?[N+1];
 
-            // Set first person to Group A
-            inGroup[1] = false;
-            return PossibleBipartition_IsBipartite(adjacencyDictionary, wasVisited, inGroup, 1);
+            // Initialize first person
+            wasVisited[1] = true;
+            inGroup[1]    = false;
+
+            for (var i = 1; i <= N; i++)
+            {
+                if (!PossibleBipartition_IsBipartite(adjacencyDictionary, wasVisited, inGroup, i))
+                    return false;
+            }
+            
+            return true;
         }
 
-        private bool PossibleBipartition_IsBipartite(Dictionary<int, List<int>> adjacencyDictionary, bool[] wasVisited, bool?[] inGroup, int person1)
+        private bool PossibleBipartition_IsBipartite(Dictionary<int, List<int>> adjacencyDictionary, bool[] wasVisited, bool?[] inGroup, int personA)
         {
-            foreach(var person2 in adjacencyDictionary[person1])
+            if (!adjacencyDictionary.ContainsKey(personA))
+                return true;
+
+            foreach(var personB in adjacencyDictionary[personA])
             {
-                if (!wasVisited[person2])
+                if (!wasVisited[personB])
                 {
-                    wasVisited[person2] = true;
+                    wasVisited[personB] = true;
 
-                    inGroup[person2] = !inGroup[person1];
+                    inGroup[personB] = !inGroup[personA];
 
-                    if (!PossibleBipartition_IsBipartite(adjacencyDictionary, wasVisited, inGroup, person2))
+                    if (!PossibleBipartition_IsBipartite(adjacencyDictionary, wasVisited, inGroup, personB))
                         return false;
                 }
-                else if (inGroup[person1] == inGroup[person2])
+                else if (inGroup[personA] == inGroup[personB])
                     return false;
             }
 
@@ -116,10 +129,10 @@ namespace LeetCode
             // Test case 66/66 (https://leetcode.com/submissions/detail/345505746/)
             N = 5;
             dislikes = new int[][] {
-                new int[] {1,2},
-                new int[] {3,4},
-                new int[] {4,5},
-                new int[] {3,5}
+                new int[] { 1, 2 },
+                new int[] { 3, 4 },
+                new int[] { 4, 5 },
+                new int[] { 3, 5 }
             };
             Assert.False(s.PossibleBipartition(N, dislikes));
         }
