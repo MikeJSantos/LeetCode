42081cdf04b817ca724d86e8f5bc446bbc3beca3
Michael Santos <michael.joseph.santos@gmail.com>
2020-05-28 17:02:01 -0400

    FirstBadVersion disabled test until API call is mocked/faked

--- Solutions/May_Challenge/Week1/FirstBadVersion.cs
+++ Solutions/May_Challenge/Week1/FirstBadVersion.cs
@@ -1,7 +1,5 @@
 using Xunit;
 
-/* The isBadVersion API is defined in the parent class VersionControl.
-      bool IsBadVersion(int version); */
 namespace LeetCode
 {
     public partial class Solution
@@ -34,22 +32,24 @@ namespace LeetCode
                 return maxVersion + 1;
         }
 
+        /* The isBadVersion API is defined in the parent class VersionControl.
+            bool IsBadVersion(int version); */
         private bool IsBadVersion(int version)
-        {
+        {   // TODO: Mock/fake API call
             return version >= 1150769282;
         }
     }
 
     public partial class UnitTests
     {
-        [Fact]
+        // [Fact] // Disabled until IsBadVersion() is mocked/faked
         public void FirstBadVersionTest()
         {
             var s = new Solution();
 
-            Assert.Equal(4, s.FirstBadVersion(5));
-            Assert.Equal(3, s.FirstBadVersion(4));
-            Assert.Equal(1150769282, s.FirstBadVersion(1420736637));
+            // Assert.Equal(4, s.FirstBadVersion(5));
+            // Assert.Equal(3, s.FirstBadVersion(4));
+            // Assert.Equal(1150769282, s.FirstBadVersion(1420736637));
         }
     }
 }
