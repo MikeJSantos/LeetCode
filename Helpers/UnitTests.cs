b7908bc4d4ae0c69044413c946b81d6b710522b4
Michael Santos <michael.joseph.santos@gmail.com>
2020-05-30 12:50:32 -0400

    +Create2dArray()

--- Helpers/UnitTests.cs
+++ Helpers/UnitTests.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using Xunit;
 
 namespace LeetCode
 {
@@ -13,6 +15,43 @@ namespace LeetCode
             {
                 return sr.ReadToEnd();
             }
+        }
+
+        private int[][] Create2dArray(int elementsPerRow, params int[] inputs)
+        {
+            var numRows = (int) Math.Ceiling((double) inputs.Length/elementsPerRow);
+            var array = new int[numRows][];
+
+            for (var rowIdx = 0; rowIdx < numRows; rowIdx++)
+            {
+                var sourceIndex = rowIdx * elementsPerRow;
+                var length = (sourceIndex + elementsPerRow <= inputs.Length)
+                    ? elementsPerRow
+                    : inputs.Length - elementsPerRow;
+                var row = new int[length];
+                Array.Copy(inputs, sourceIndex, row, 0, length);
+                array[rowIdx] = row;
+            }
+            
+            return array;
+        }
+
+        [Fact]
+        public void Create2dArrayTest()
+        {
+            int[][] expected;
+
+            expected = new int[][] {
+                new int[] { 1, 2 },
+                new int[] { 3, 4 }
+            };
+            Assert.Equal(expected, Create2dArray(2, 1, 2, 3, 4));
+
+            expected = new int[][] {
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5 }
+            };
+            Assert.Equal(expected, Create2dArray(3, 1, 2, 3, 4, 5));
         }
     }
 }
