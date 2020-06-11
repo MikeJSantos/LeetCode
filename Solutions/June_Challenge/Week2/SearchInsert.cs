d166ac54c19780380ad950620fdc5d7446daa018
Michael Santos <michael.joseph.santos@gmail.com>
2020-06-11 13:50:42 -0400

    SearchInsert with faster algorithm

--- Solutions/June_Challenge/Week2/SearchInsert.cs
+++ Solutions/June_Challenge/Week2/SearchInsert.cs
@@ -1,23 +1,36 @@
+using System;
 using Xunit;
 
 namespace LeetCode
 {
     public partial class Solution
     {
-        // TODO: Optimize with divide & conquer solution. Beats 6.47% of submissions
-        // https://leetcode.com/submissions/detail/351874201
+        // TODO: Optimize Beats 57.41% of submissions
+        // https://leetcode.com/submissions/detail/352256037
         public int SearchInsert(int[] nums, int target)
         {
-            if (nums.Length == 0)
+            if (nums.Length == 0 || nums[0] > target)
                 return 0;
+            if (nums[nums.Length-1] < target)
+                return nums.Length;
 
-            for (var i = 0; i < nums.Length; i++)
+            int low = 0,
+                high = nums.Length - 1,
+                mid;
+
+            while (low < high)
             {
-                if (nums[i] >= target)
-                    return i;
+                mid = low + (high - low) / 2;
+
+                if (nums[mid] == target)
+                    return mid;
+                else if (nums[mid] < target)
+                    low = (low != mid) ? mid : low + 1;
+                else
+                    high = (high != mid) ? mid : high - 1;
             }
 
-            return nums.Length;
+            return low;
         }
     }
 
@@ -48,6 +61,18 @@ namespace LeetCode
             nums     = new int[] { 1, 3, 5, 6 };
             target   = 0;
             expected = 0;
+            Assert.Equal(expected, s.SearchInsert(nums, target));
+
+            // Test case 27/62 https://leetcode.com/submissions/detail/352251373/
+            nums     = new int[] { 2, 7, 8, 9, 10 };
+            target   = 9;
+            expected = 3;
+            Assert.Equal(expected, s.SearchInsert(nums, target));
+
+            // Test case 52/62 https://leetcode.com/submissions/detail/352256863
+            nums = new int[] { 1, 3, 5, 6 };
+            target   = 7;
+            expected = 4;
             Assert.Equal(expected, s.SearchInsert(nums, target));
         }
     }
